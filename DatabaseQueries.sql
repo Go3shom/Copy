64d6a83592a28496364bb8e9ddacab90ec806424
Go3shom <shadi.muhammad001@gmail.com>
2019-02-04 18:41:18 +0200

    Update Database Agian

--- DatabaseQueries.sql
+++ DatabaseQueries.sql
@@ -8,16 +8,18 @@ CREATE TABLE IF NOT EXISTS Customers (
     CustomerAddress varchar(150) NOT NULL
 );
 
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Ana Trujillo','Obere Str. 57');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Antonio Moreno','Avda. de la Constitución 2222');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Thomas Hardy','Mataderos 2312');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Christina Berglund','120 Hanover Sq.');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Hanna Moos','Berguvsvägen 8');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Francisco Chang','Sierras de Granada 9993');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Yang Wang','Hauptstr. 29');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Laurence Lebihans','12, rue des Bouchers');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Elizabeth Lincoln','23 Tsawassen Blvd.');
-INSERT INTO Customers( CustomerName, CustomerAddress ) VALUES ('Victoria Ashworth','Fauntleroy Circus');
+INSERT INTO Customers( CustomerID, CustomerName, CustomerAddress ) VALUES ( 139, 'Ana Trujillo','Obere Str. 57');
+INSERT INTO Customers( CustomerID, CustomerName, CustomerAddress ) VALUES ( 465, 'Antonio Moreno','Avda. de la Constitución 2222');
+INSERT INTO Customers( CustomerID, CustomerName, CustomerAddress ) VALUES (765,'Thomas Hardy','Mataderos 2312');
+INSERT INTO Customers( CustomerID,  CustomerName, CustomerAddress ) VALUES (231,'Christina Berglund','120 Hanover Sq.');
+INSERT INTO Customers( CustomerID,  CustomerName, CustomerAddress ) VALUES (468,'Hanna Moos','Berguvsvägen 8');
+INSERT INTO Customers( CustomerID,  CustomerName, CustomerAddress ) VALUES (978,'Francisco Chang','Sierras de Granada 9993');
+INSERT INTO Customers( CustomerID, CustomerName, CustomerAddress ) VALUES ( 487,'Yang Wang','Hauptstr. 29');
+INSERT INTO Customers( CustomerID, CustomerName, CustomerAddress ) VALUES (981, 'Laurence Lebihans','12, rue des Bouchers');
+INSERT INTO Customers( CustomerID,  CustomerName, CustomerAddress ) VALUES (667,'Elizabeth Lincoln','23 Tsawassen Blvd.');
+INSERT INTO Customers( CustomerID, CustomerName, CustomerAddress ) VALUES (239, 'Victoria Ashworth','Fauntleroy Circus');
+
+
 
 
 CREATE DATABASE CopiedCopmany;
